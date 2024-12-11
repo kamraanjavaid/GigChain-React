6fd28898abddf94b76f3af24d2e12c8666250b8e
Kamran Javaid <133736391+kamraanjavaid@users.noreply.github.com>
2024-12-12 04:00:26 +0500

    fix (index): add back the frontend url to the CORS function

--- backend/index.js
+++ backend/index.js
@@ -40,12 +40,28 @@ const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: true,
+//     credentials: true,
+//   })
+// );
+
+app.use(cors({
+    origin: ["https://gigchain-frontend.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://gigchain-frontend.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 
 mongoose
   .connect(process.env.MONGODB_URI, {})
